4a195fb4dbc0bebf52da93e56ce703d3b072201f
Davide Vitelaru <davide@davidevitelaru.com>
2017-06-29 18:38:06 +0100

    Made script output result to stdout

--- src/main.py
+++ src/main.py
@@ -2,33 +2,18 @@
 Polling is handled externally by calling this script
 This script will output the required information ONCE and append timestamps
 '''
-import os
+import os, boto3, time, datetime
 from inventory_manager import InventoryManager
 
 def main():
+    session = boto3.Session() # will be created with EC2 role
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 
+    inventory_manager = InventoryManager(session)
+    inventory = inventory_manager.list_inventory()
 
-
-    session = create_session()
-    inventory_manager = InventoryManager()
-
-'''
-Creates a boto3 session using the given execution role
-'''
-def create_session(self, execution_role):
-
-    sts_client = boto3.client('sts')
-    assumedRoleObject = sts_client.assume_role(
-        RoleArn=execution_role,
-        RoleSessionName="AssumeRoleSession"
-    )
-    credentials = assumedRoleObject['Credentials']
-    boto3_session = boto3.Session( aws_access_key_id = credentials['AccessKeyId'],
-        aws_secret_access_key = credentials['SecretAccessKey'],
-        aws_session_token = credentials['SessionToken']
-        )
-
-    return boto3_session
+    for item in inventory:
+        print timestamp + " " + item["name"] + " " + item["type"] + " " + item["region"]
 
 if __name__ == '__main__':
     main()
