a455b55bb87a32be47cb049cd3b11d9c6442dce9
Davide Vitelaru <davide@davidevitelaru.com>
2017-07-02 16:02:20 +0100

    Implemented /aggregate API method

--- server.py
+++ server.py
@@ -1,9 +1,42 @@
+import os, sys, datetime
+from datetime import timedelta
 from flask import Flask
+from flask import jsonify
+from models import Instance, InstanceRecord
+
 app = Flask(__name__)
 
 @app.route('/aggregate')
 def get_aggregate():
-   return 'Hello'
+
+    result = {}
+
+    # Retrieving records for all instances in the last day
+    aggregate_data = Instance.objects(records__timestamp__gte=datetime.datetime.now() - timedelta(days=1))
+
+    for instance in aggregate_data:
+
+        # We have zone but we want region
+        instance_region = instance["instance_zone"][:-1]
+
+        # Adding region to results if it does not exist
+        if instance_region not in result:
+            result[instance_region] = {}
+
+        # Adding type into region if it does not exist
+        if instance["instance_type"] not in result[instance_region]:
+            result[instance_region][instance["instance_type"]] = 1
+        # If it does, simply incrementing the counter
+        else:
+            result[instance_region][instance["instance_type"]] += 1
+
+    return jsonify(result)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+
+    if 'CM_DB_URI' not in os.environ or 'CM_DB_NAME' not in os.environ:
+        sys.stderr.write("\033[91mCM_DB_URI and CM_DB_NAME system variables are required\033[0m\n")
+        sys.stderr.write("\033[91mApplication will now exit\033[0m\n")
+        exit(1)
+
+    app.run(debug = True)
--- test_api.py
+++ test_api.py
@@ -1,4 +1,5 @@
-import unittest, os, datetime
+import unittest, os, datetime, random, json
+from datetime import timedelta
 from server import app
 
 # Database environment variables need exporting before
@@ -7,42 +8,50 @@ os.environ['CM_DB_URI'] = 'mongodb://localhost'
 os.environ['CM_DB_NAME'] = 'test_db'
 from models import Instance, InstanceRecord
 
+# Keeping the same 'now' time so that DB queries won't be "1 month and 1 second"
+# away from making it into the query
+LOAD_TIME = datetime.datetime.now()
+
 class TestAPIMethods(unittest.TestCase):
 
-    '''
-    Setting up the test data
-    '''
     @classmethod
     def setUpClass(self):
-        # Creating a test client for Flask
+        '''
+        Creating a test client for Flask
+        '''
         self.app = app.test_client()
         self.app.testing = True
 
         # Clearing test DB
         Instance.drop_collection()
 
-        # Setting up 60 instances
-        create = {
-            "eu-west-1a": {
-                "t2.micro": 5,
-                "t2.nano": 20
-            },
-            "eu-west-1b": {
-                "t2.micro": 8,
-                "m4.xlarge": 7
-            },
-            "eu-west-1c": {
-                "t2.micro": 9
-            },
-            "eu-west-2a": {
-                "t2.micro": 11
-            }
-        }
+        '''
+        Populating database with random test data
+        '''
+        MAX_INSTANCES = 60
+        zones = [ "eu-west-1a", "eu-west-1b", "eu-west-1c", "eu-west-2a"]
+        types = [ "t2.micro", "t2.nano", "m4.xlarge" ]
+        to_create = {}
 
+        # Creating random information from sample regions and types
+        for index in range(0, MAX_INSTANCES):
+
+            random_zone = zones[random.randint(0, len(zones) - 1)]
+            random_type = types[random.randint(0, len(types) - 1)]
+
+            if random_zone not in to_create:
+                to_create[random_zone] = {}
+
+            if random_type not in to_create[random_zone]:
+                to_create[random_zone][random_type] = 1
+            else:
+                to_create[random_zone][random_type] += 1
+
+        # Writing instance records to database based on random information
         instance_id_counter = 0
-        for zone in create:
-            for itype in create[zone]:
-                for count in range(0,create[zone][itype]):
+        for zone in  to_create:
+            for itype in  to_create[zone]:
+                for count in range(0, to_create[zone][itype]):
                     instance = Instance(
                         instance_type = itype,
                         instance_id = "i-" + str(instance_id_counter),
@@ -54,20 +63,71 @@ class TestAPIMethods(unittest.TestCase):
 
         # Spreading instance records across 12 months
         timestamps = []
-        for i in range(-12,0):
-            timestamps.append(monthdelta(datetime.datetime.now(), i))
+        for i in range(-11,1):
+            timestamps.append(monthdelta(LOAD_TIME, i))
+
+        # Going through all instances and creating records between two
+        # randomly-selected timestamps
+        for instance in Instance.objects():
+            start = random.randint(0, len(timestamps) - 2)
+            end = random.randint(start + 1, len(timestamps) - 1)
+
+            for timestamp_index in range(start, end + 1):
+                instance.records.append(InstanceRecord(
+                    timestamp = timestamps[timestamp_index],
+                    instance_state = "RUNNING"
+                ))
+                instance.save()
 
     def setUp(self):
         pass
 
     '''
     Should return total number of servers per region and instance type
+    for the last day
     '''
     def test_aggregate(self):
 
-        response = self.app.get('/aggregate')
+        '''
+        Calculating expected result
+        Format:
+        {
+            'region': {
+                'type': count
+            }
+        }
+        '''
+        results = Instance.objects(records__timestamp__gte=(LOAD_TIME - timedelta(days=1)))
+        expected = {}
 
-        self.assertEqual('Hello', response.data)
+        for instance in results:
+            # We have zone but we want region
+            instance_region = instance["instance_zone"][:-1]
+            if instance_region not in expected:
+                expected[instance_region] = {}
+            if instance["instance_type"] not in expected[instance_region]:
+                expected[instance_region][instance["instance_type"]] = 1
+            else:
+                expected[instance_region][instance["instance_type"]] += 1
+
+        '''
+        Making call and validating output
+        '''
+        raw_response = self.app.get('/aggregate')
+        response = json.loads(raw_response.data)
+
+        for expected_region in expected:
+            # Region must be present in response
+            self.assertIn(expected_region, response)
+
+            for itype in expected[expected_region]:
+                # Instance type must be present in expected region
+                self.assertIn(itype, response[expected_region])
+                # Counts must match
+                self.assertEquals(
+                        expected[expected_region][itype],
+                        response[expected_region][itype]
+                    )
 
     def test_aggregate_region(self):
         pass
