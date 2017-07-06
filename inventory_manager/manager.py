a04b919fae0444cfd4e1f606355df9f9964e10ed
Tom Marks <thomas.marks@gmail.com>
2017-07-06 11:02:47 -0400

    Simplifies list_inventory with a list comprehension

--- inventory_manager/manager.py
+++ inventory_manager/manager.py
@@ -7,38 +7,32 @@ class InventoryManager():
         self.account = self.session.client('sts').get_caller_identity().get('Account')
 
     def list_inventory(self):
-
         ec2 = self.session.client('ec2')
         raw_inventory = ec2.describe_instances()
 
-        inventory = []
-
-        for reservation in raw_inventory["Reservations"]:
-            for instance in reservation["Instances"]:
-
-                instance_name = self.get_name(instance)
-
-                # All keys are mandatory, if one of them is missing
-                # the application will crash, but we'd have bigger problems
-                inventory.append({
-                    'name': instance_name,
-                    'id': instance["InstanceId"],
-                    'type': instance["InstanceType"],
-                    'state': instance["State"]["Name"],
-                    'zone': instance["Placement"]["AvailabilityZone"],
-                    'account': self.account
-                })
-
-        return inventory
+        return [
+            # All keys are mandatory, if one of them is missing
+            # the application will crash, but we'd have bigger problems
+            {
+                'name': self.get_name(instance),
+                'id': instance["InstanceId"],
+                'type': instance["InstanceType"],
+                'state': instance["State"]["Name"],
+                'region': instance["Placement"]["AvailabilityZone"],
+                'zone': instance["Placement"]["AvailabilityZone"],
+                'account': self.account
+            }
+            for reservation in raw_inventory["Reservations"]
+            for instance in reservation["Instances"]
+        ]
 
     def get_name(self, instance):
         """
-        Returns an instance's name.
+        Return an instance's name.
 
         Default value is '<Nameless> to makes output easier
         to process or query
         """
-
         for tag in instance.get("Tags", []):
             if tag["Key"] == "Name":
                 return tag["Value"]
