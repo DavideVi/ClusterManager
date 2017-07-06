619211a4f26391b7173974d927f12ba2f77656c7
Tom Marks <thomas.marks@gmail.com>
2017-07-06 10:16:03 -0400

    Adds coverage reporting when running tests.
    Note to enable successful coverage reporting the inventory_manager.py
    module had to be turned into a package.

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,9 @@
 export AWS_DEFAULT_REGION=us-east-1
-nosetests test_inventory_manager.py test_models.py
+nosetests \
+    --with-coverage \
+    --cover-package=inventory_manager \
+    --cover-html \
+    --cover-branches \
+    --cover-min-percentage=80 \
+    test_inventory_manager.py test_models.py
 # python test_api.py
